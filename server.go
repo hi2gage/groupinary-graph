002911efb5e64b3cbf65a1832c106efec57ba0a7
hi2gage <hi2gage.pdx@gmail.com>
2023-06-10 20:53:55 -0600

    added heroku.yml

--- server.go
+++ server.go
@@ -5,17 +5,18 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strings"
 
 	"shrektionary_api/ent"
 	"shrektionary_api/ent/migrate"
 	"shrektionary_api/graph"
+	"shrektionary_api/middleware"
 
 	"entgo.io/ent/dialect"
 	_ "github.com/lib/pq" // Import the PostgreSQL driver package
 
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
-	"github.com/rs/cors"
 )
 
 const (
@@ -56,17 +57,55 @@ func main() {
 
 	srv := handler.NewDefaultServer(graph.NewSchema(client))
 
-	corsOptions := cors.Options{
-		AllowedOrigins:   []string{"https://studio.apollographql.com"}, // or "*" for wildcard
-		AllowCredentials: true,
+	// Middleware for logging requests
+	loggingMiddleware := func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			// Log the incoming request
+			log.Printf("Received request: %s %s", r.Method, r.URL.Path)
+
+			// Log the headers for preflight request or actual request
+			if r.Method == http.MethodOptions {
+				log.Println("Preflight Request Headers:")
+			} else {
+				log.Println("Actual Request Headers:")
+			}
+
+			// Print each header key-value pair
+			for name, values := range r.Header {
+				// Join multiple values for the same header with commas
+				value := strings.Join(values, ", ")
+				log.Printf("%s: %s", name, value)
+			}
+			log.Printf("==========================================================================================")
+
+			// Call the next handler
+			next.ServeHTTP(w, r)
+		})
 	}
 
-	corsHandler := cors.New(corsOptions).Handler(srv)
+	// corsOptions := cors.Options{
+	// 	AllowedOrigins:   []string{"studio.apollographql.com"}, // or "*" for wildcard
+	// 	AllowCredentials: true,
+	// }
+	// corsHandler := cors.New(corsOptions)
+	corsHandler := func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			// Set the Access-Control-Allow-Origin header
+			w.Header().Set("Access-Control-Allow-Origin", "https://studio.apollographql.com")
 
-	// authMiddleware = middleware.EnsureValidToken()
+			// Set other necessary CORS headers if needed
+			w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+			w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+
+			// Call the next handler
+			next.ServeHTTP(w, r)
+		})
+	}
+
+	authMiddleware := middleware.EnsureValidToken()
 
 	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
-	http.Handle("/query", corsHandler)
+	http.Handle("/query", loggingMiddleware(corsHandler(authMiddleware(srv))))
 
 	log.Printf("connect to https://localhost:%s/ for GraphQL playground", port)
 	log.Fatal(http.ListenAndServeTLS(":"+port, defaultCertFile, defaultKeyFile, nil))
