1ce9b5866a23023bb2afa9286eaa2046db11aeff
hi2gage <hi2gage.pdx@gmail.com>
2023-09-26 02:20:32 -0600

    Change the packages around

--- graph/resolvers/ent.resolvers.go
+++ graph/resolvers/ent.resolvers.go
@@ -1,4 +1,4 @@
-package graph
+package resolvers
 
 // This file will be automatically regenerated based on the schema, any resolver implementations
 // will be copied through when generating and any unknown code will be moved to the end.
@@ -6,52 +6,44 @@ package graph
 
 import (
 	"context"
+	"fmt"
 	"shrektionary_api/ent"
+	"shrektionary_api/graph"
 
 	"entgo.io/contrib/entgql"
 )
 
 // Node is the resolver for the node field.
 func (r *queryResolver) Node(ctx context.Context, id int) (ent.Noder, error) {
-	return r.client.Noder(ctx, id)
+	panic(fmt.Errorf("not implemented: Node - node"))
 }
 
 // Nodes is the resolver for the nodes field.
 func (r *queryResolver) Nodes(ctx context.Context, ids []int) ([]ent.Noder, error) {
-	return r.client.Noders(ctx, ids)
+	panic(fmt.Errorf("not implemented: Nodes - nodes"))
 }
 
 // Definitions is the resolver for the definitions field.
 func (r *queryResolver) Definitions(ctx context.Context, after *entgql.Cursor[int], first *int, before *entgql.Cursor[int], last *int, orderBy *ent.DefinitionOrder, where *ent.DefinitionWhereInput) (*ent.DefinitionConnection, error) {
-	return r.client.Definition.Query().
-		Paginate(ctx, after, first, before, last,
-			ent.WithDefinitionOrder(ent.DefaultDefinitionOrder),
-			ent.WithDefinitionFilter(where.Filter),
-			ent.WithDefinitionOrder(orderBy),
-		)
+	panic(fmt.Errorf("not implemented: Definitions - definitions"))
 }
 
 // Groups is the resolver for the groups field.
 func (r *queryResolver) Groups(ctx context.Context) ([]*ent.Group, error) {
-	return r.client.Group.Query().All(ctx)
+	panic(fmt.Errorf("not implemented: Groups - groups"))
 }
 
 // Users is the resolver for the users field.
 func (r *queryResolver) Users(ctx context.Context) ([]*ent.User, error) {
-	return r.client.User.Query().All(ctx)
+	panic(fmt.Errorf("not implemented: Users - users"))
 }
 
 // Words is the resolver for the words field.
 func (r *queryResolver) Words(ctx context.Context, after *entgql.Cursor[int], first *int, before *entgql.Cursor[int], last *int, orderBy *ent.WordOrder, where *ent.WordWhereInput) (*ent.WordConnection, error) {
-	return r.client.Word.Query().
-		Paginate(ctx, after, first, before, last,
-			ent.WithWordOrder(ent.DefaultWordOrder),
-			ent.WithWordFilter(where.Filter),
-			ent.WithWordOrder(orderBy),
-		)
+	panic(fmt.Errorf("not implemented: Words - words"))
 }
 
-// Query returns QueryResolver implementation.
-func (r *Resolver) Query() QueryResolver { return &queryResolver{r} }
+// Query returns graph.QueryResolver implementation.
+func (r *Resolver) Query() graph.QueryResolver { return &queryResolver{r} }
 
 type queryResolver struct{ *Resolver }
