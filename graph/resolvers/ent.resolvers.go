74f1f6ae25766840d9ed959b19137204541a7727
hi2gage <hi2gage.pdx@gmail.com>
2023-09-30 16:43:30 -0600

    Fix Panics

--- graph/resolvers/ent.resolvers.go
+++ graph/resolvers/ent.resolvers.go
@@ -6,7 +6,6 @@ package resolvers
 
 import (
 	"context"
-	"fmt"
 	"shrektionary_api/ent"
 	"shrektionary_api/graph"
 
@@ -15,32 +14,42 @@ import (
 
 // Node is the resolver for the node field.
 func (r *queryResolver) Node(ctx context.Context, id int) (ent.Noder, error) {
-	panic(fmt.Errorf("not implemented: Node - node"))
+	return r.client.Noder(ctx, id)
 }
 
 // Nodes is the resolver for the nodes field.
 func (r *queryResolver) Nodes(ctx context.Context, ids []int) ([]ent.Noder, error) {
-	panic(fmt.Errorf("not implemented: Nodes - nodes"))
+	return r.client.Noders(ctx, ids)
 }
 
 // Definitions is the resolver for the definitions field.
 func (r *queryResolver) Definitions(ctx context.Context, after *entgql.Cursor[int], first *int, before *entgql.Cursor[int], last *int, orderBy *ent.DefinitionOrder, where *ent.DefinitionWhereInput) (*ent.DefinitionConnection, error) {
-	panic(fmt.Errorf("not implemented: Definitions - definitions"))
+	return r.client.Definition.Query().
+		Paginate(ctx, after, first, before, last,
+			ent.WithDefinitionOrder(ent.DefaultDefinitionOrder),
+			ent.WithDefinitionFilter(where.Filter),
+			ent.WithDefinitionOrder(orderBy),
+		)
 }
 
 // Groups is the resolver for the groups field.
 func (r *queryResolver) Groups(ctx context.Context) ([]*ent.Group, error) {
-	panic(fmt.Errorf("not implemented: Groups - groups"))
+	return r.client.Group.Query().All(ctx)
 }
 
 // Users is the resolver for the users field.
 func (r *queryResolver) Users(ctx context.Context) ([]*ent.User, error) {
-	panic(fmt.Errorf("not implemented: Users - users"))
+	return r.client.User.Query().All(ctx)
 }
 
 // Words is the resolver for the words field.
 func (r *queryResolver) Words(ctx context.Context, after *entgql.Cursor[int], first *int, before *entgql.Cursor[int], last *int, orderBy *ent.WordOrder, where *ent.WordWhereInput) (*ent.WordConnection, error) {
-	panic(fmt.Errorf("not implemented: Words - words"))
+	return r.client.Word.Query().
+		Paginate(ctx, after, first, before, last,
+			ent.WithWordOrder(ent.DefaultWordOrder),
+			ent.WithWordFilter(where.Filter),
+			ent.WithWordOrder(orderBy),
+		)
 }
 
 // Query returns graph.QueryResolver implementation.
