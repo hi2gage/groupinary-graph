77d5ddfdb8d0601b65658427a84445681c35dc80
hi2gage <hi2gage.pdx@gmail.com>
2023-11-28 00:14:40 -0700

    add TestConnectWords

--- testutils/testutils.go
+++ testutils/testutils.go
@@ -7,6 +7,8 @@ import (
 	"database/sql"
 	"groupinary/ent"
 	"groupinary/utils"
+	"os"
+	"testing"
 	"time"
 
 	entsql "entgo.io/ent/dialect/sql"
@@ -15,38 +17,8 @@ import (
 	"github.com/go-testfixtures/testfixtures/v3"
 )
 
-func OpenTest(paths ...string) (*ent.Client, error) {
-	// Use SQLite in-memory database for testing.
-	db, err := sql.Open("sqlite3", "file:test:memory:?cache=shared&_fk=1")
-	if err != nil {
-		return nil, err
-	}
-	db.SetMaxIdleConns(10)
-	db.SetMaxOpenConns(100)
-	db.SetConnMaxLifetime(time.Hour)
-
-	// Create an ent.Driver from `db`.
-	drv := entsql.OpenDB("sqlite3", db)
-
-	// Run migrations.
-	client := ent.NewClient(ent.Driver(drv))
-	if err := runMigrations(client); err != nil {
-		return nil, err
-	}
-
-	// Load fixtures.
-	if err := loadFixtures(db, paths...); err != nil {
-		return nil, err
-	}
-
-	return client, nil
-}
-
-func runMigrations(client *ent.Client) error {
-	return client.Schema.Create(context.Background(), schema.WithDropIndex(true))
-}
-
-func loadFixtures(db *sql.DB, paths ...string) error {
+// LoadFixtures loads fixtures into the provided database.
+func LoadFixtures(db *sql.DB, paths ...string) error {
 	fixtures, err := testfixtures.New(
 		testfixtures.Database(db),
 		testfixtures.Dialect("sqlite3"),
@@ -64,6 +36,46 @@ func loadFixtures(db *sql.DB, paths ...string) error {
 	return nil
 }
 
+// OpenTest opens a new test environment with an SQLite in-memory database,
+// runs migrations, and loads fixtures.
+func OpenTest() (*ent.Client, *sql.DB, error) {
+	// Use SQLite in-memory database for testing.
+	dbPath := "file:test:memory:?_fk=1"
+	db, err := sql.Open("sqlite3", dbPath)
+	if err != nil {
+		return nil, nil, err
+	}
+	db.SetMaxIdleConns(10)
+	db.SetMaxOpenConns(100)
+	db.SetConnMaxLifetime(time.Hour)
+
+	// Create an ent.Driver from `db`.
+	drv := entsql.OpenDB("sqlite3", db)
+
+	// Run migrations.
+	client := ent.NewClient(ent.Driver(drv))
+	if err := runMigrations(client); err != nil {
+		return nil, nil, err
+	}
+
+	return client, db, nil
+}
+
+func runMigrations(client *ent.Client) error {
+	return client.Schema.Create(context.Background(), schema.WithDropIndex(true))
+}
+
 func TestContext(userID int) context.Context {
 	return utils.AddUserIdToContext(context.Background(), userID)
+}
+
+// CleanupTestEnvironment performs cleanup for the test environment, including
+// closing the client and removing the SQLite in-memory database file.
+func CleanupTestEnvironment(t *testing.T, client *ent.Client) {
+	if err := client.Close(); err != nil {
+		t.Error(err)
+	}
+	if err := os.Remove("test:memory:"); err != nil {
+		t.Errorf("Error removing database file: %v", err)
+	}
 }
