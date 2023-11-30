7eb36f873e5cc45e79d58135ffe7af761183db24
hi2gage <hi2gage.pdx@gmail.com>
2023-11-29 23:08:42 -0700

    add tests to resolvers (#20)

--- graph/resolvers/resolver.go
+++ graph/resolvers/resolver.go
@@ -15,11 +15,6 @@ import (
 // Resolver is the resolver root.
 type Resolver struct{ client *ent.Client }
 
-// Hello resolves the "hello" query.
-func (r *Resolver) Hello() string {
-	return "Hello, world!"
-}
-
 // NewSchema creates a graphql executable schema.
 func NewSchema(client *ent.Client) graphql.ExecutableSchema {
 	return graph.NewExecutableSchema(graph.Config{
