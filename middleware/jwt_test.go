4a6f966e4c5e248c5cd44ccd19a0d1d46e04ad4e
hi2gage <hi2gage.pdx@gmail.com>
2023-12-23 23:03:22 -0700

    Merge Develop into Main (#31)
    * move around the jwt
    
    * retrigger checks

--- middleware/jwt_test.go
+++ middleware/jwt_test.go
@@ -4,8 +4,9 @@ import (
 	"context"
 	"net/http"
 	"net/http/httptest"
-	"net/url"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestCustomClaimsValidate(t *testing.T) {
@@ -32,18 +33,6 @@ func TestCustomClaimsValidate(t *testing.T) {
 			}
 		})
 	}
-}
-
-func TestParseIssuerURL(t *testing.T) {
-	t.Run("Valid Issuer URL", func(t *testing.T) {
-		expectedURL := "https://dev-afmzazq3cr35ktpl.us.auth0.com/"
-		issuerURL := parseIssuerURL()
-
-		// Assert that the parsed URL matches the expected URL
-		if issuerURL.String() != expectedURL {
-			t.Errorf("Expected issuer URL %s, got %s", expectedURL, issuerURL.String())
-		}
-	})
 }
 
 func TestHandleValidationError(t *testing.T) {
@@ -84,47 +73,99 @@ func someValidationErrorFunction() error {
 func TestSetupJWTValidator(t *testing.T) {
 	testCases := []struct {
 		name           string
-		issuerURL      *url.URL
-		expectedError  bool
+		jwtENV         EnvJWTStruct
+		expectedError  string
 		expectedNilJWT bool
 	}{
 		{
-			name:           "Valid JWT Validator",
-			issuerURL:      mustParseURL("https://mock-auth0-issuer.com/"),
-			expectedError:  false,
+			name: "Valid JWT Validator",
+			jwtENV: EnvJWTStruct{
+				IssuerURL: "https://mock-auth0-issuer.com/",
+				Audience: []string{
+					"https://shrektionary.com/api",
+					"4W01gsxupS4xoLLxbe8jdVVlGTFOKjd3",
+				},
+			},
+			expectedError:  "",
 			expectedNilJWT: false,
 		},
 		{
-			name:           "Error Creating JWT Validator",
-			issuerURL:      mustParseURL(""), // Invalid URL to trigger an error
-			expectedError:  true,
+			name: "Empty String URL",
+			jwtENV: EnvJWTStruct{
+				IssuerURL: "",
+				Audience:  []string{},
+			},
+			expectedError:  "failed to set up the JWT validator: issuer url is required but was empty",
 			expectedNilJWT: true,
 		},
-		// Add more test cases as needed
 	}
 
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
-			jwtValidator, err := setupJWTValidator(tc.issuerURL)
+			jwtValidator, err := setupJWTValidator(tc.jwtENV)
 
-			// Check for expected error
-			if (err != nil) != tc.expectedError {
-				t.Errorf("Expected error: %v, got error: %v", tc.expectedError, err)
-			}
-
-			// Check for expected nil JWT validator
-			if (jwtValidator == nil) != tc.expectedNilJWT {
-				t.Errorf("Expected nil JWT validator: %v, got: %v", tc.expectedNilJWT, jwtValidator)
+			if err != nil {
+				assert.Error(t, err, "Expected error")
+				assert.Contains(t, err.Error(), tc.expectedError, "Error message should contain expected string")
+				assert.NotEqual(t, "", tc.expectedError, "expectedError should not be an empty string // Got: %v", err)
+				assert.Nil(t, jwtValidator, "jwtValidator should be nil when there is no error // Got: %v", jwtValidator)
+			} else {
+				assert.Equal(t, "", tc.expectedError, "expectedError should be empty // Got: %v", tc.expectedError)
+				assert.NotNil(t, jwtValidator, "jwtValidator should not be nil when there is no error")
 			}
 		})
 	}
 }
 
-// Helper function to parse URL and panic on error (for test simplicity)
-func mustParseURL(rawURL string) *url.URL {
-	parsedURL, err := url.Parse(rawURL)
-	if err != nil {
-		panic(err)
-	}
-	return parsedURL
-}
+// // MockUserOperations is a mock implementation of UserOperations for testing purposes.
+// type MockUserOperations struct {
+// 	mock.Mock
+// }
+
+// func (m *MockUserOperations) CheckUserExists(authID string) (*int, error) {
+// 	args := m.Called(authID)
+// 	value := args.Int(0)
+// 	return &value, args.Error(1)
+// }
+
+// func (m *MockUserOperations) AddUserToGraph(authID string) (*int, error) {
+// 	args := m.Called(authID)
+// 	value := args.Int(0)
+// 	return &value, args.Error(1)
+// }
+
+// func TestEnsureValidToken_ErrorDuringSetup(t *testing.T) {
+// 	mockClient := new(MockUserOperations)
+
+// 	// mockValidator := &validator.Validator{} // You can use a mock validator if needed
+
+// 	// Simulate an error during setup
+// 	mockEnv := EnvJWTStruct{
+// 		IssuerURL: "https://dev-afmzazq3cr35ktpl.us.auth0.com/",
+// 		Audience: []string{
+// 			"https://shrektionary.com/api",
+// 			"4W01gsxupS4xoLLxbe8jdVVlGTFOKjd3",
+// 		},
+// 	}
+
+// 	// Call the EnsureValidToken function
+// 	middleware := EnsureValidToken(mockClient, mockEnv)
+
+// 	// Create a test request and response
+// 	req := httptest.NewRequest("GET", "/query", nil)
+// 	w := httptest.NewRecorder()
+
+// 	// Call the middleware with the test request and response
+// 	// middleware(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {})).ServeHTTP(w, req)
+
+// 	mockClient.On("CheckUserExists", mock.Anything).Return(0, nil).Once()
+// 	mockClient.On("AddUserToGraph", mock.Anything).Return(0, nil).Once()
+
+// 	// Call the middleware with the test request and response
+// 	middleware(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+// 		// Your test logic here
+// 	})).ServeHTTP(w, req)
+
+// 	// Assert that the expectations were met
+// 	mockClient.AssertExpectations(t)
+// }
