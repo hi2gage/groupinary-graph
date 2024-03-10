0ec377abc94defd739fede02400f302a9e28526a
hi2gage <hi2gage.pdx@gmail.com>
2024-03-10 11:14:03 -0600

    Update ent/migrate/migrations/20240310170818_migration_name.sql
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- ent/migrate/migrations/20240310170818_migration_name.sql
+++ ent/migrate/migrations/20240310170818_migration_name.sql
@@ -1,2 +1,9 @@
+-- atlas:txtar
+
+-- checks/destructive.sql --
+-- atlas:assert DS103
+SELECT NOT EXISTS (SELECT 1 FROM "words" WHERE "group_root_words" IS NOT NULL) AS "is_empty";
+
+-- migration.sql --
 -- Modify "words" table
 ALTER TABLE "words" DROP COLUMN "group_root_words";
